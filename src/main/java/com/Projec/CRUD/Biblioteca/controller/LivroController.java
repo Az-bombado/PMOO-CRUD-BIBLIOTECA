98faab3d505ce8a669248a13664f9abd3ee71f7f
Dani <danielctalevi@gmail.com>
2026-06-02 18:56:39 -0300

    Botão de editar livro funciona e a aba livros tem como voltar para a aba Home

--- src/main/java/com/Projec/CRUD/Biblioteca/controller/LivroController.java
+++ src/main/java/com/Projec/CRUD/Biblioteca/controller/LivroController.java
@@ -2,10 +2,12 @@ package com.Projec.CRUD.Biblioteca.controller;
 
 import com.Projec.CRUD.Biblioteca.model.Livro;
 import com.Projec.CRUD.Biblioteca.repository.LivroRepository;
-import org.springframework.ui.Model;
-import java.util.List;
+
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,19 +19,62 @@ public class LivroController {
     @Autowired
     private LivroRepository livroRepository;
 
-    @PostMapping
-    @ResponseBody
-    public Livro salvarLivro(@RequestBody Livro livro) {
-        return livroRepository.save(livro);
-    }
+    // LISTAR LIVROS
 
     @GetMapping
-    public String mostrarPagina(Model model){
+    public String mostrarPagina(Model model,
+                                HttpSession session){
 
-        List<Livro> livros = livroRepository.findAll();
+        Object usuario =
+                session.getAttribute("usuarioLogado");
+
+        if(usuario == null){
+            return "redirect:/login";
+        }
+
+        List<Livro> livros =
+                livroRepository.findAll();
 
         model.addAttribute("livros", livros);
 
         return "livros/livros";
+    }
+
+    // SALVAR LIVRO
+
+    @PostMapping("/salvar")
+    public String salvarLivro(Livro livro){
+
+        livroRepository.save(livro);
+
+        return "redirect:/livros";
+    }
+
+    // EXCLUIR LIVRO
+
+    @GetMapping("/deletar/{id}")
+    public String deletarLivro(@PathVariable Long id){
+
+        livroRepository.deleteById(id);
+
+        return "redirect:/livros";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editarLivro(@PathVariable Long id, Model model){
+
+        Livro livro = livroRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Livro não encontrado"));
+
+        model.addAttribute("livro", livro);
+
+        return "livros/editar";
+    }
+    @PostMapping("/atualizar")
+    public String atualizarLivro(Livro livro){
+
+        livroRepository.save(livro);
+
+        return "redirect:/livros";
     }
 }
--- src/main/resources/static/js/livros/livros.js
+++ src/main/resources/static/js/livros/livros.js
@@ -1,1 +1,35 @@
-console.log("Sistema de Biblioteca carregado!");
+document.addEventListener("DOMContentLoaded", () => {
+
+    const modal = document.getElementById("modal");
+
+    window.abrirModal = function () {
+        modal.style.display = "flex";
+    }
+
+    window.fecharModal = function () {
+        modal.style.display = "none";
+    }
+
+    const pesquisa = document.getElementById("pesquisa");
+
+    pesquisa.addEventListener("keyup", () => {
+
+        const valor = pesquisa.value.toLowerCase();
+
+        const linhas =
+            document.querySelectorAll("#tabelaLivros tr");
+
+        linhas.forEach(linha => {
+
+            const texto =
+                linha.innerText.toLowerCase();
+
+            linha.style.display =
+                texto.includes(valor)
+                    ? ""
+                    : "none";
+        });
+
+    });
+
+});
